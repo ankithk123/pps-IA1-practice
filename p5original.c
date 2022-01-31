1b12454b545e190ce7a31983e1713ccc705b694c
Ankith  K <96900989+ankithk123@users.noreply.github.com>
2022-01-31 06:54:52 +0000

    p5original.c

---
+++ p5original.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+
+float input()
+{
+  float n;
+  printf("Enter a number to find the square root of:");
+  scanf("%f",&n);
+  return n;
+}
+float squareroot(float n)
+{
+  float x=n;
+  float y=1;
+  float e=0.000001;
+  while(x-y>e)
+  {
+    x=(x+y)/2;
+    y=n/x;
+  }
+  return x;
+}
+void output(float n,float sres)
+{
+  printf("Square root of given input %0.2f is =%0.2f\n",n,sres);
+}
+int main()
+{
+  float n,sres;
+  n=input();
+  sres=squareroot(n);
+  output(n,sres);
+  return 0;
+}
